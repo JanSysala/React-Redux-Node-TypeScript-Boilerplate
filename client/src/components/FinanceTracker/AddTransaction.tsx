cfed9512434a4184832d2ef7186ac0bcd8e4409d
JanSysala <jan.sysala@outlook.com>
2020-05-01 16:41:04 +0200

    Removed Material-UI from AddTransaction component and fixed NaN error in amount input.

--- client/src/components/FinanceTracker/AddTransaction.tsx
+++ client/src/components/FinanceTracker/AddTransaction.tsx
@@ -1,37 +1,10 @@
 import * as React from 'react';
 import {ChangeEvent, useState} from 'react';
 import {useDispatch} from 'react-redux';
-import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import AddIcon from '@material-ui/icons/Add';
 import {addTransaction, resetError} from "../../actions";
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            padding: '2px 4px',
-            display: 'flex',
-            alignItems: 'center',
-            width: 400,
-        },
-        input: {
-            marginLeft: theme.spacing(1),
-            flex: 1,
-        },
-        iconButton: {
-            padding: 10,
-        },
-        divider: {
-            height: 28,
-            margin: 4,
-        },
-    }),
-);
 
 export const AddTransaction: React.FC = () => {
-    const classes = useStyles();
     // create some local state with useState that will tell us if our fetch result is still loading
     // const [fetching, setFetching] = useState(false);
     const [text, setText] = useState('');
@@ -62,24 +35,25 @@ export const AddTransaction: React.FC = () => {
     }
 
     return (
-        <Paper component="form" className={classes.root} onSubmit={onSubmit}>
-            <InputBase
+        <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                aria-label={'Add transaction text.'}
                 value={text}
-                className={classes.input}
                 placeholder="Transaction text"
                 onChange={(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setText(e.target.value)}
-                inputProps={{'aria-label': 'Add transaction text.'}}
             />
-            <InputBase
+            <input
+                type="text"
+                pattern="[0-9]*"
+                aria-label={'Add transaction amount.'}
                 value={amount}
-                className={classes.input}
                 placeholder="Amount"
-                onChange={(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setAmount(+e.target.value)}
-                inputProps={{'aria-label': 'Add transaction amount.'}}
+                onChange={(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setAmount(parseInt(e.target.value) || 0)}
             />
-            <IconButton type="submit" className={classes.iconButton} aria-label="add">
-                <AddIcon/>
-            </IconButton>
-        </Paper>
+            <button type="submit" aria-label="add">
+                +
+            </button>
+        </form>
     );
 };
